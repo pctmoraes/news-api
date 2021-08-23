0bd6877cd798e72f081a5747ffea4f87619f7309
Paula Moraes <paula.ct.moraes@gmail.com>
2021-08-23 16:25:11 -0300

    Add controller and news controller

---
+++ controller/newsController.ts
@@ -0,0 +1,13 @@
+import NewsService from "../services/newsService";
+
+class NewsController {
+    get(req, res) {}
+
+    getById(req, res) {}
+
+    create(req, res) {}
+
+    update(req, res) {}
+
+    delete(req, res) {}
+}
