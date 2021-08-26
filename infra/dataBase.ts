38172c0716a67ddad6f1ae8cd4c32fce8472bd4c
Paula Moraes <paula.ct.moraes@gmail.com>
2021-08-26 17:00:40 -0300

    Add docker file and configurations

--- infra/dataBase.ts
+++ infra/dataBase.ts
@@ -1,7 +1,7 @@
 import * as mongoose from 'mongoose';
 
 class Database {
-    private DB_URL = 'mongodb://localhost:27017/db_portal';
+    private DB_URL = 'mongodb://link-db/db_portal';
 
     createConnection() {
         mongoose.connect(this.DB_URL);
