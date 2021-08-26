ab5bc701d423ad3e47212cf4cd14a65bfc911bab
Paula Moraes <paula.ct.moraes@gmail.com>
2021-08-26 17:35:06 -0300

    Code refactored

--- infra/dataBase.ts
+++ infra/dataBase.ts
@@ -1,9 +1,13 @@
 import * as mongoose from 'mongoose';
 
 class Database {
-    private DB_URL = 'mongodb://link-db/db_portal';
+    private DB_URL = 'mongodb://link-db:3050/db_portal';
 
     createConnection() {
+        mongoose.set('useNewUrlParser', true);
+        mongoose.set('useFindAndModify', false);
+        mongoose.set('useCreateIndex', true);
+        mongoose.set('useUnifiedTopology', true);
         mongoose.connect(this.DB_URL);
     }
 }
